3171ec7c73b6305e9e367075204c98e321d517ae
leeyonghyun <mi161776@hanbat.ac.kr>
2019-05-25 23:24:04 +0900

    Fix Wrong Credients Error code

--- src/exceptions/WrongCredentialsException.ts
+++ src/exceptions/WrongCredentialsException.ts
@@ -2,7 +2,7 @@ import HttpException from './HttpException';
 
 class WrongCredentialsException extends HttpException {
   constructor() {
-    super(401, `Wrong Credentials!`);
+    super(403, `Wrong Credentials!`);
   }
 }
 
