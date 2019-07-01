f92d3d040f76ba91ca52fdf0e89b6a38a70aad9d
leeyonghyun <mi161776@hanbat.ac.kr>
2019-07-01 20:55:16 +0900

    multi upload implement

--- src/controllers/pictureController.ts
+++ src/controllers/pictureController.ts
@@ -2,22 +2,36 @@ import Controller from "@interface/controller.interface";
 import * as express from "express";
 import RequestWithUser from "@interface/requestWithUser.interface";
 import {authMiddleware} from "@middleware/auth.middleware";
+import * as multer from "multer";
+import * as path from "path";
 
 class PictureController implements Controller {
   public path = '/picture';
   public router = express.Router();
+  public storage: multer.StorageEngine;
+  public upload: multer.Instance;
 
   constructor() {
+    this.storage = multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, path.join(__dirname + '../../../uploads'))
+      },
+      filename: function (req, file, cb) {
+        console.log(file);
+        cb(null, Date.now() + '-' + file.originalname)
+      }
+    });
+    this.upload = multer({ storage: this.storage });
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
     this.router.use(authMiddleware);
-    this.router.post(`${this.path}/`, this.uploadPictures);
+    this.router.post(`${this.path}/`, this.upload.array('picture'),this.uploadPictures);
   }
 
   private uploadPictures = async (req: RequestWithUser, res: express.Response, next: express.NextFunction) => {
-    console.log(req.user);
+    console.log(req.files);
     res.json(req.user);
   };
 }
